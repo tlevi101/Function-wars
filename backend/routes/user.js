d2a13b0a30194eb673106bdc1a2e5beb00ccc098
“levente.torma” <leventetorma3@gmail.com>
2023-04-26 13:20:57 +0200

    friend request route fix, friends controller

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,192 +3,23 @@ const router = express.Router();
 const { User, Friendship, Report, Chat } = require('../models');
 const auth = require('../middlewares/auth');
 const { Op } = require('sequelize');
+const {FriendsController} = require("../types/controllers/FriendsController");
 
-router.get('/friends/:online?', auth, async (req, res) => {
-    const { email } = req.user;
-    const { online } = req.params;
-    const user = await User.findOne({ where: { email: email } });
-    let friends = await user.getFriends();
-    friends = await Promise.all(
-        friends.map(async friend => {
-            const chat = await friend.getChat(user.id);
-            let unreadMessages = 0;
-            if (chat) {
-                unreadMessages = chat.messages.filter(
-                    message => message.seen === false && message.from != user.id
-                ).length;
-            }
-            return { name: friend.name, id: friend.id, unreadMessages: unreadMessages };
-        })
-    );
-    console.debug(online)
-    if(online) {
-        friends = friends.filter(friend => !!req.onlineUsers.get(friend.id));
-    }
-    res.status(200).json({ friends: friends });
-});
+router.get('/friends', auth, FriendsController.getFriends);
 
-router.get('/friends/requests', auth, async (req, res) => {
-    const { email } = req.user;
-    const user = await User.findOne({ where: { email: email } });
-    if (!user) {
-        return res.status(404).json({ message: 'User not found.' });
-    }
-    let requests = await user.getFriendRequests();
-    requests = requests.map(request => {
-        return { id: request.id, from: { id: request.from.id, name: request.from.name } };
-    });
-    res.status(200).json({ requests: requests });
-});
+router.get('/friends/online', auth, FriendsController.getOnlineFriends);
 
-router.get('/friends/:id/chat', auth, async (req, res) => {
-    const { email } = req.user;
-    const { id } = req.params;
-    const user = await User.findOne({ where: { email: email } });
-    if (!user) {
-        return res.status(404).json({ message: 'User not found.' });
-    }
-    const friend = await User.findOne({ where: { id: id } });
-    if (!friend) {
-        return res.status(404).json({ message: 'Friend not found.' });
-    }
-    const friendship = await Friendship.findOne({
-        where: {
-            [Op.or]: [
-                { user_id: user.id, friend_id: friend.id },
-                { user_id: friend.id, friend_id: user.id },
-            ],
-            pending: false,
-        },
-    });
-    if (!friendship) {
-        return res.status(404).json({ message: 'Friendship not found.' });
-    }
-    let chat = await user.getChat(friend.id);
-    if (!chat) {
-        let messages = [];
-        chat = await Chat.create({ friendship_id: friendship.id, messages: messages });
-    }
-    res.status(200).json({ chat: chat });
-});
+router.get('/friends/requests', auth, FriendsController.getFriendRequests);
 
-router.put('/friends/requests/:id/accept', auth, async (req, res) => {
-    const { email } = req.user;
-    const { id } = req.params;
-    console.log(email);
-    console.log(id);
-    const user = await User.findOne({ where: { email: email } });
-    if (!user) {
-        return res.status(404).json({ message: 'User not found.' });
-    }
-    const friendship = await Friendship.findOne({
-        where: {
-            id: id,
-        },
-    });
-    if (!friendship) {
-        return res.status(404).json({ message: 'Friendship request not found.' });
-    }
-    if (friendship.friend_id !== user.id) {
-        return res.status(403).json({
-            message: 'You are not the recipient of this friendship request.',
-        });
-    }
-    await friendship.update({ pending: false });
-    res.status(200).json({ message: 'Friendship accepted.' });
-});
+router.get('/friends/:id/chat', auth, FriendsController.getFriendChat);
 
-router.delete('/friends/requests/:id/reject', auth, async (req, res) => {
-    const { email } = req.user;
-    const { id } = req.params;
-    const user = await User.findOne({ where: { email: email } });
-    if (!user) {
-        return res.status(404).json({ message: 'User not found.' });
-    }
-    const friendship = await Friendship.findOne({
-        where: {
-            id: id,
-        },
-    });
-    if (!friendship) {
-        return res.status(404).json({ message: 'Friendship request not found.' });
-    }
-    if (friendship.friend_id !== user.id) {
-        return res.status(403).json({
-            message: 'You are not the recipient of this friendship request.',
-        });
-    }
-    await friendship.destroy();
-    res.status(200).json({ message: 'Friendship rejected.' });
-});
+router.put('/friends/requests/:id/accept', auth, FriendsController.acceptFriendRequest);
 
-router.delete('/friends/:id', auth, async (req, res) => {
-    const userJWT = req.user;
-    const user = await User.findOne({ where: { email: userJWT.email } });
-    if (!user) {
-        return res.status(404).json({ message: 'User not found.' });
-    }
-    const { id } = req.params;
-    const friendship = await Friendship.findOne({
-        where: {
-            [Op.or]: [
-                { friend_id: id, user_id: user.id },
-                { friend_id: user.id, user_id: id },
-            ],
-        },
-    });
-    if (!friendship) {
-        return res.status(404).json({ message: 'Friendship not found.' });
-    }
-    if (friendship.pending) {
-        return res.status(403).json({ message: "You can't delete a pending friendship." });
-    }
-    if (friendship.friend_id !== user.id && friendship.user_id !== user.id) {
-        return res.status(403).json({
-            message: 'You are not the recipient of this friendship request.',
-        });
-    }
-    const chat = await friendship.getChat();
-    if (chat) {
-        await Chat.destroy({
-            where: {
-                id: chat.id,
-            },
-        });
-    }
-    await Friendship.destroy({
-        where: {
-            id: friendship.id,
-        },
-    });
-    res.status(200).json({ message: 'Friend deleted.' });
-});
+router.delete('/friends/requests/:id/reject', auth, FriendsController.rejectFriendRequest);
 
-router.post('/friends/:id', auth, async (req, res) => {
-    const { name } = req.user;
-    const { id } = req.params;
-    const user = await User.findOne({ where: { name: name } });
-    if (user.id === id) {
-        return res.status(403).json({ message: "You can't add yourself as a friend." });
-    }
-    const friendship = await Friendship.findOne({
-        where: {
-            [Op.or]: [
-                { friend_id: id, user_id: user.id },
-                { friend_id: user.id, user_id: id },
-            ],
-        },
-    });
-    if (friendship) {
-        return res.status(403).json({ message: 'Friendship already exists.' });
-    }
-    const newFriendship = await Friendship.create({
-        user_id: user.id,
-        friend_id: id,
-        pending: true,
-    });
-    res.status(200).json({ message: 'Friendship request sent.' });
-});
+router.delete('/friends/:id', auth, FriendsController.deleteFriend);
+
+router.post('/friends/:id', auth, FriendsController.sendFriendRequest);
 
 router.post('/users/:id/report', auth, async (req, res) => {
     const { name } = req.user;
