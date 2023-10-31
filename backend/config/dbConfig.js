4fdfdc29bad6b1bf05177a2a465a72ce9d9dbe38
levente.torma <leventetorma3@gmail.com>
2023-10-31 16:26:43 +0100

    added cert to prod db connection

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -15,6 +15,9 @@ const dbConfig = {
         password: process.env.DB_PASSWORD,
         dialect: 'mysql',
         logging: process.env.DB_LOGGING === 'true',
+		ssl: {
+			ca: fs.readFileSync(__dirname + '../certs/DigiCertGlobalRootCA.crt.pem')
+		}
     },
     test: {
         host: process.env.DB_HOST,
